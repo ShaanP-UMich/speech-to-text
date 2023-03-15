44eda7d7802cca65bf39796c465fab1bab1eb589
Shaan P <shaanp@umich.edu>
2023-03-15 03:35:49 -0400

    Got rid of broken record files, renamed the working record file to main.py

--- main.py
+++ main.py
@@ -1,88 +1,42 @@
 import pyaudio
 import wave
-from pynput import keyboard
+import keyboard
 
-# set up the audio parameters
-chunk = 100000  # number of audio samples per frame
-sample_format = pyaudio.paInt16  # 16-bit resolution
-channels = 1  # mono
-fs = 44100  # sampling rate
-seconds_per_file = 5  # number of seconds to record per file
+CHUNK = 8182
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+WAVE_OUTPUT_FILENAME = "output.wav"
 
-# set up the PyAudio stream
 p = pyaudio.PyAudio()
-# print(p.get_default_input_device_info())
 
-stream = p.open(format=sample_format,
-                channels=channels,
-                rate=fs,
-                frames_per_buffer=chunk,
-                input=True)
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
-# set up a counter for the files
-file_counter = 1
+frames = []
+is_recording = False
 
-# start recording when the 'r' key is pressed
-
-
-def on_press(key):
-    print("pressed key")
-    print(key)
-    try:
-        # print('alphanumeric key {0} pressed'.format(
-        #     key.char))
-        if key.char == 'r':
-            print("'R' key was pressed")
-            # listener.stop()  # stop listening for 'r' key
-            record()
-            return False
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
-
-
-def on_release(key):
-    print('released key')
-    if key.char == 'q':
-        # listener.stop_flag = True
-        return False
-
-
-def record():
-    global file_counter
-    frames = []  # buffer to hold the audio frames
-    while True:
-        data = stream.read(chunk)
+while True:
+    if keyboard.is_pressed('space'):
+        if not is_recording:
+            print("Recording started...")
+            is_recording = True
+        data = stream.read(CHUNK)
         frames.append(data)
-        if listener.stop_flag:  # stop recording when 'q' key is pressed
-            listener.stop_flag = False  # reset the flag for next time
-            break
+    elif is_recording:
+        print("Recording stopped.")
+        break
 
-    # save the audio file
-    wf = wave.open(f"recording{file_counter}.wav", 'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(sample_format))
-    wf.setframerate(fs)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    # increment the file counter
-    file_counter += 1
-
-    # start listening for 'r' key again
-    # listener.start()
-
-
-# create a keyboard listener to detect the 'r' key press
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-listener.start()
-
-print("Press 'R' to start recording and 'Q' to stop recording")
-
-# wait for the listener to finish
-listener.join()
-
-# clean up the PyAudio stream
 stream.stop_stream()
 stream.close()
 p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
