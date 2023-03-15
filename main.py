03b80c634cf3fff5c6dc39073e32c8fd9b35a7a1
Shaan Parikh <shaanp@umich.edu>
2023-03-15 15:29:31 -0400

    Program allows you to record multiple voice clips. Can stop program by pressing 'q'

--- main.py
+++ main.py
@@ -4,57 +4,90 @@ import keyboard
 import uuid
 import hashlib
 import time
+import threading
 
-audio_uuid = uuid.uuid4()
-sha256 = hashlib.sha256(str(audio_uuid).encode('utf-8')).hexdigest()
-short_uuid = sha256[:8]
 
-CHUNK = 4096  # 8182
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-WAVE_OUTPUT_FILENAME = f"output-{short_uuid}.wav"
+def record_clip(record_flag):
+    while record_flag[0]:
+        audio_uuid = uuid.uuid4()
+        sha256 = hashlib.sha256(str(audio_uuid).encode('utf-8')).hexdigest()
+        short_uuid = sha256[:8]
 
-p = pyaudio.PyAudio()
+        CHUNK = 4096  # 8182
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        WAVE_OUTPUT_FILENAME = f"output-{short_uuid}.wav"
 
-# print(pyaudio.PyAudio().get_device_count())
-# exit()
+        p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+        # print(pyaudio.PyAudio().get_device_count())
+        # exit()
 
-frames = []
-is_recording = False
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
 
-print("Hold 'space' to start recording. Release 'space' to stop recording")
+        frames = []
+        is_recording = False
 
-while True:
-    if keyboard.is_pressed('space'):
-        if not is_recording:
-            print("Recording started...")
-            is_recording = True
-        data = stream.read(CHUNK)
-        frames.append(data)
-    elif is_recording:
-        start_time = time.time()
-        while time.time() - start_time < 1:
-            data = stream.read(CHUNK)
-            frames.append(data)
-            
-        print("Recording stopped.")
-        print(f"Outputted to file {WAVE_OUTPUT_FILENAME}")
-        break
+        print("Hold 'space' to start recording. Release 'space' to stop recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+        while True:
+            if keyboard.is_pressed('space'):
+                if not is_recording:
+                    print("Recording started...")
+                    is_recording = True
+                data = stream.read(CHUNK)
+                frames.append(data)
+            elif is_recording:
+                start_time = time.time()
+                while time.time() - start_time < 1:
+                    data = stream.read(CHUNK)
+                    frames.append(data)
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+                print("Recording stopped.")
+                print(f"Outputted to file {WAVE_OUTPUT_FILENAME}")
+                break
+            elif not record_flag[0]:
+                print("Recording manually stopped")
+                stream.stop_stream()
+                stream.close()
+                p.terminate()
+                return
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+
+def on_q(record_flag):
+    print("'q' key was pressed")
+    record_flag[0] = False
+    exit(0)
+
+
+def main():
+    print("main function")
+
+    record_flags = [True]
+
+    keyboard.add_hotkey('q', on_q, args=[record_flags])
+
+    recording_thread = threading.Thread(
+        target=record_clip, args=(record_flags,))
+
+    recording_thread.start()
+
+
+if __name__ == "__main__":
+    main()
